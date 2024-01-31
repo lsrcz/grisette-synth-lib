4a02bd8ac3e25162cd11468f87cc892273c264cc
Sirui Lu <siruilu@cs.washington.edu>
2024-01-31 15:46:59 -0800

    :memo: Add more documentation for arith

--- arith/Main.hs
+++ arith/Main.hs
@@ -29,20 +29,41 @@ type ConProg = Concrete.Prog OpCode Integer OpType
 
 type Sketch = Component.Prog OpCode SymInteger OpType
 
--- The sketch is currently all manually constructed and the constants need to be
--- assigned with unique names manually. A generator for this is under
--- development.
+-- The sketch is currently all manually constructed for the purpose of showing
+-- the structure of a sketch. The symbolic constants are assigned with unique
+-- names manually (e.g. @stmt1'arg0@).
+--
+-- A generator for a sketch with components is under development.
 sketch :: Sketch
 sketch =
   Component.Prog
+    -- The name of the program. If your program supports procedure calls, you
+    -- should make sure that the programs has a unique name.
     "test"
+    -- The arguments to the program with types. "x" and "y" are names of the
+    -- arguments and is only used for pretty printing purpose for now.
     [ Component.ProgArg IntegerType "x",
       Component.ProgArg IntegerType "y"
     ]
+    -- The components of the program. Each component is a statement.
+    --
+    -- The synthesizer could
+    -- * reorder the components, and
+    -- * choose whether or now to disable a component, and
+    -- * choose the arguments of a component.
     [ Component.Stmt
-        Plus
+        Minus
+        -- These are the symbolic constants controlling the arguments of the
+        -- statement.
         ["stmt0'arg0", "stmt0'arg1"]
+        -- This is a symbolic constants controling the reordering of the
+        -- components.
+        --
+        -- The components would be sorted based on them. Each component can only
+        -- refer to the outputs of the components defined before.
         ["stmt0'ret0"]
+        -- This is a symbolic boolean constant controlling whether a statement
+        -- is disabled.
         "stmt0'dis",
       Component.Stmt
         Mul
@@ -50,11 +71,13 @@ sketch =
         ["stmt1'ret0"]
         "stmt1'dis",
       Component.Stmt
-        Minus
+        Plus
         ["stmt2'arg0", "stmt2'arg1"]
         ["stmt2'ret0"]
         "stmt2'dis"
     ]
+    -- The program result. Use the symbolic constant @r@ to choose which
+    -- statement is used as the result.
     [Component.ProgRes IntegerType "r"]
 
 -- The specification specifies the expected behavior. Here, we want to synthesis
@@ -76,6 +99,7 @@ main = do
         SynthesisWithFuzzerTask
           { synthesisWithFuzzerTaskSymProg = sketch,
             synthesisWithFuzzerTaskSpec = spec,
+            -- You need a working z3 installation available in your PATH.
             synthesisWithFuzzerTaskSolverConfig = precise z3,
             synthesisWithFuzzerTaskSemantics = Sem,
             synthesisWithFuzzerTaskMaxTests = 100,
