233b379f004e9e6fcceda196df3f14d85cbf1714
Sirui Lu <siruilu@cs.washington.edu>
2024-04-15 09:11:10 -0700

    :sparkles: Add getSubProg for UnionM op

--- src/Grisette/Lib/Synth/Program/SubProg.hs
+++ src/Grisette/Lib/Synth/Program/SubProg.hs
@@ -1,9 +1,23 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Grisette.Lib.Synth.Program.SubProg (HasSubProg (..)) where
 
+import Grisette (Mergeable, MonadUnion, UnionM, liftUnionM)
 import Grisette.Lib.Synth.Context (MonadContext)
 
 class (MonadContext ctx) => HasSubProg op subProg ctx | op -> subProg where
   getSubProg :: op -> ctx [subProg]
+
+instance
+  ( MonadContext ctx,
+    MonadUnion ctx,
+    HasSubProg op subProg ctx,
+    Mergeable subProg,
+    Mergeable op
+  ) =>
+  HasSubProg (UnionM op) subProg ctx
+  where
+  getSubProg op = liftUnionM op >>= getSubProg
