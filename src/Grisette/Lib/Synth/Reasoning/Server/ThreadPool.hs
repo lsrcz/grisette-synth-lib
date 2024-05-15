78a8a46f4807db0f51684f1a96553cf0e0b250e8
Sirui Lu <siruilu@cs.washington.edu>
2024-05-15 12:07:20 -0700

    :recycle: Refactor the thread pool

--- src/Grisette/Lib/Synth/Reasoning/Server/ThreadPool.hs
+++ src/Grisette/Lib/Synth/Reasoning/Server/ThreadPool.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE ExplicitNamespaces #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Grisette.Lib.Synth.Reasoning.Server.ThreadPool
   ( TaskHandle (taskHandleId),
@@ -26,33 +28,42 @@ import Control.Concurrent.Async (Async, async, cancelWith)
 import Control.Concurrent.STM
   ( STM,
     TMVar,
-    TVar,
     atomically,
-    modifyTVar',
     newEmptyTMVarIO,
-    newTVarIO,
     readTMVar,
-    readTVar,
-    readTVarIO,
     tryPutTMVar,
     tryReadTMVar,
   )
 import Control.Exception
-  ( Exception (toException),
+  ( Exception (fromException, toException),
     SomeException,
     catch,
     mask,
     throwIO,
   )
-import Control.Monad (void, when)
-import Data.Dynamic (Dynamic, Typeable, fromDyn, toDyn)
+import Control.Monad (when)
 import qualified Data.HashMap.Lazy as HM
 import Data.Hashable (Hashable (hashWithSalt))
 import Data.IORef (IORef, atomicModifyIORef', newIORef, readIORef, writeIORef)
 import qualified Data.Map.Ordered as OM
+import Data.Typeable (Typeable, eqT, type (:~:) (Refl))
 import GHC.IO (unsafePerformIO)
 
-newtype PendingTask = PendingTask {task :: IO Dynamic}
+data CancellingException where
+  CancellingException :: (Exception e) => e -> CancellingException
+
+instance Show CancellingException where
+  show (CancellingException e) = show e
+
+instance Exception CancellingException
+
+data PendingTask a = PendingTask
+  { task :: IO a,
+    taskResultSlot :: TMVar (Either SomeException a)
+  }
+
+data SomePendingTask where
+  SomePendingTask :: (Typeable a) => PendingTask a -> SomePendingTask
 
 taskIdCounter :: IORef Int
 taskIdCounter = unsafePerformIO $ newIORef 0
@@ -66,15 +77,18 @@ freshTaskId = atomicModifyIORef' taskIdCounter (\x -> (x + 1, x))
 data Pool = Pool
   { lock :: MVar (),
     poolSize :: Int,
-    running :: IORef (HM.HashMap TaskId (Async Dynamic)),
-    queue :: IORef (OM.OMap TaskId PendingTask),
-    results :: TVar (HM.HashMap TaskId (TMVar (Either SomeException Dynamic)))
+    running :: IORef (HM.HashMap TaskId (Async ())),
+    queue :: IORef (OM.OMap TaskId SomePendingTask)
   }
 
 numOfRunningTasks :: Pool -> IO Int
 numOfRunningTasks Pool {..} = HM.size <$> readIORef running
 
-data TaskHandle a = TaskHandle {taskHandleId :: TaskId, taskPool :: Pool}
+data TaskHandle a = TaskHandle
+  { taskHandleId :: TaskId,
+    taskPool :: Pool,
+    taskResult :: TMVar (Either SomeException a)
+  }
 
 instance Eq (TaskHandle a) where
   TaskHandle {taskHandleId = a} == TaskHandle {taskHandleId = b} = a == b
@@ -108,22 +122,23 @@ newPool :: Int -> IO Pool
 newPool poolSize = do
   lock <- newMVar ()
   running <- newIORef HM.empty
-  results <- newTVarIO HM.empty
   queue <- newIORef OM.empty
   return Pool {..}
 
-taskFun :: Pool -> TaskId -> IO Dynamic -> IO Dynamic
-taskFun pool@Pool {..} taskId task = mask $ \restore -> do
-  let writeResult result = withLockedPool pool True $ do
+taskFun :: Pool -> TaskId -> TMVar (Either SomeException a) -> IO a -> IO ()
+taskFun pool@Pool {..} taskId resultTMVar task = mask $ \restore -> do
+  let writeResult result = do
         oldRunning <- readIORef running
         writeIORef running $ HM.delete taskId oldRunning
-        oldResults <- readTVarIO results
-        atomically $ tryPutTMVar (oldResults HM.! taskId) result
+        atomically $ tryPutTMVar resultTMVar result
   result <-
-    restore task `catch` \(e :: SomeException) -> do
-      writeResult (Left e) >> throwIO e
-  writeResult (Right result)
-  return result
+    restore task `catch` \(e :: SomeException) ->
+      case fromException e of
+        Just (CancellingException e1) ->
+          writeResult (Left $ toException e1) >> throwIO e1
+        Nothing -> withLockedPool pool True $ writeResult (Left e) >> throwIO e
+  withLockedPool pool True $ writeResult (Right result)
+  return ()
 
 startIfHaveSpaceImpl :: Pool -> IO ()
 startIfHaveSpaceImpl pool@Pool {..} = do
@@ -131,42 +146,39 @@ startIfHaveSpaceImpl pool@Pool {..} = do
   when (HM.size oldRunning < poolSize) $ do
     oldQueue <- readIORef queue
     case OM.elemAt oldQueue 0 of
-      Just (taskId, PendingTask {..}) -> do
-        oldResults <- readTVarIO results
-        result <- atomically $ tryReadTMVar (oldResults HM.! taskId)
+      Just (taskId, SomePendingTask (PendingTask {..})) -> do
+        exception <- atomically $ tryReadTMVar taskResultSlot
         writeIORef queue $ OM.delete taskId oldQueue
-        case result of
+        case exception of
           Just _ -> return ()
           Nothing -> do
-            taskAsync <- async $ taskFun pool taskId task
+            taskAsync <- async $ taskFun pool taskId taskResultSlot task
             writeIORef running $ HM.insert taskId taskAsync oldRunning
         startIfHaveSpaceImpl pool
       _ -> return ()
 
-addNewTask :: (Typeable a) => Pool -> IO a -> IO (TaskHandle a)
-addNewTask pool@Pool {..} taskBase = withLockedPool pool True $ do
+addNewTask :: forall a. (Typeable a) => Pool -> IO a -> IO (TaskHandle a)
+addNewTask pool@Pool {..} task = withLockedPool pool True $ do
   taskId <- freshTaskId
+  taskResultSlot <- newEmptyTMVarIO :: IO (TMVar (Either SomeException a))
   oldQueue <- readIORef queue
-  let task = toDyn <$> taskBase
-  writeIORef queue $ oldQueue OM.>| (taskId, PendingTask {..})
-  result <- newEmptyTMVarIO
-  atomically $ modifyTVar' results $ HM.insert taskId result
+  writeIORef queue $ oldQueue OM.>| (taskId, SomePendingTask $ PendingTask {..})
   startIfHaveSpaceImpl pool
-  return $ TaskHandle taskId pool
+  return $ TaskHandle taskId pool taskResultSlot
 
 cancelTaskWithImpl :: (Exception e) => Pool -> e -> TaskId -> IO ()
-cancelTaskWithImpl Pool {..} e taskId = do
+cancelTaskWithImpl pool@Pool {..} e taskId = do
   oldRunning <- readIORef running
-  oldResults <- readTVarIO results
-  atomically $ tryPutTMVar (oldResults HM.! taskId) (Left $ toException e)
+  oldQueue <- readIORef queue
   case HM.lookup taskId oldRunning of
-    Just taskAsync ->
-      -- Cancellation is asynchronous, however, from the view of pollTask or
-      -- waitCatchTask, the task is already cancelled.
-      --
-      -- This is to avoid deadlock.
-      void $ async $ cancelWith taskAsync e
-    Nothing -> return ()
+    Just taskAsync -> do
+      cancelWith taskAsync $ CancellingException e
+      startIfHaveSpaceImpl pool
+    Nothing -> case OM.lookup taskId oldQueue of
+      Just (SomePendingTask PendingTask {..}) -> do
+        atomically $ tryPutTMVar taskResultSlot (Left $ toException e)
+        return ()
+      _ -> return ()
 
 cancelTaskWith :: (Exception e) => e -> TaskHandle a -> IO ()
 cancelTaskWith e taskHandle@TaskHandle {taskPool = pool@Pool {..}} =
@@ -176,40 +188,46 @@ cancelAllTasksWith :: (Exception e) => Pool -> e -> IO ()
 cancelAllTasksWith pool@Pool {..} e = withLockedPool pool True $ do
   oldRunning <- readIORef running
   oldPending <- readIORef queue
-  mapM_ (cancelTaskWithImpl pool e) $
-    HM.keys oldRunning <> (fst <$> OM.assocs oldPending)
+  mapM_ (`cancelWith` CancellingException e) $ HM.elems oldRunning
+  mapM_
+    ( \(SomePendingTask PendingTask {..}) ->
+        atomically $ tryPutTMVar taskResultSlot (Left $ toException e)
+    )
+    $ snd <$> OM.assocs oldPending
+  startIfHaveSpaceImpl pool
 
 pollTaskSTM ::
   (Typeable a) => TaskHandle a -> STM (Maybe (Either SomeException a))
-pollTaskSTM taskHandle@TaskHandle {taskPool = Pool {..}} = do
-  oldResults <- readTVar results
-  r <- tryReadTMVar (oldResults HM.! taskHandleId taskHandle)
-  return $ fmap (fmap $ flip fromDyn (error "BUG: bad type")) r
+pollTaskSTM TaskHandle {taskResult} = tryReadTMVar taskResult
 
 pollTask :: (Typeable a) => TaskHandle a -> IO (Maybe (Either SomeException a))
 pollTask = atomically . pollTaskSTM
 
 waitCatchTaskSTM :: (Typeable a) => TaskHandle a -> STM (Either SomeException a)
-waitCatchTaskSTM taskHandle@TaskHandle {taskPool = Pool {..}} = do
-  oldResults <- readTVar results
-  r <- readTMVar (oldResults HM.! taskHandleId taskHandle)
-  return $ fmap (`fromDyn` error "BUG: bad type") r
+waitCatchTaskSTM TaskHandle {taskResult} = readTMVar taskResult
 
 waitCatchTask :: (Typeable a) => TaskHandle a -> IO (Either SomeException a)
 waitCatchTask = atomically . waitCatchTaskSTM
 
-alterTaskIfPending :: (Typeable a) => TaskHandle a -> IO a -> IO ()
+alterTaskIfPending :: forall a. (Typeable a) => TaskHandle a -> IO a -> IO ()
 alterTaskIfPending
   TaskHandle {taskHandleId, taskPool = pool@Pool {..}}
   newAction =
     withLockedPool pool False $ do
       oldQueue <- readIORef queue
       case OM.lookup taskHandleId oldQueue of
-        Just PendingTask {..} -> do
-          writeIORef queue $
-            OM.alter
-              (const $ Just $ PendingTask {task = toDyn <$> newAction})
-              taskHandleId
-              oldQueue
-          startIfHaveSpaceImpl pool
+        Just (SomePendingTask (PendingTask {..} :: PendingTask a1)) -> do
+          case eqT @a @a1 of
+            Just Refl -> do
+              writeIORef queue $
+                OM.alter
+                  ( const $
+                      Just $
+                        SomePendingTask $
+                          PendingTask {task = newAction, ..}
+                  )
+                  taskHandleId
+                  oldQueue
+              startIfHaveSpaceImpl pool
+            Nothing -> error "alterTaskIfPending: type mismatch"
         Nothing -> return ()
