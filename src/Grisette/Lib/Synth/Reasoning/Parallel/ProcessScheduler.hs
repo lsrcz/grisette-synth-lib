c9edb140423d5ef9fb5713612de1de708a5a55f7
lsrcz <lsrcz@users.noreply.github.com>
2025-01-06 00:40:35 +0000

    :art: automated ormolu commit

--- src/Grisette/Lib/Synth/Reasoning/Parallel/ProcessScheduler.hs
+++ src/Grisette/Lib/Synth/Reasoning/Parallel/ProcessScheduler.hs
@@ -2761,16 +2761,16 @@ debugLogAllStats
                         fromMaybe mempty $
                           nodeDividedChildren dcTree nid
               if
-                | null children -> return $ result <+> "{}"
-                | onlyUndetermined && nodeStatusIsDetermined status ->
-                    return $ result <+> "{...}"
-                | otherwise -> do
-                    childrenDocs <- mapM walkNode children
-                    return $
-                      vsep
-                        [ nest 2 $ vsep $ [result <+> "{", vsep childrenDocs],
-                          "}"
-                        ]
+                  | null children -> return $ result <+> "{}"
+                  | onlyUndetermined && nodeStatusIsDetermined status ->
+                      return $ result <+> "{...}"
+                  | otherwise -> do
+                      childrenDocs <- mapM walkNode children
+                      return $
+                        vsep
+                          [ nest 2 $ vsep $ [result <+> "{", vsep childrenDocs],
+                            "}"
+                          ]
         nodeResult :: NodeId -> IO (Doc ann)
         nodeResult nid = do
           state <- (HM.! nid) <$> readIORef nodeStates
