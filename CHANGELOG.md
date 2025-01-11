6258db21616fbeccfd827b475084fb3e56bc52f8
Sirui Lu <siruilu@cs.washington.edu>
2025-01-11 10:53:31 -0800

    :wrench: Use new build-env

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,8 +2,9 @@
 
 All notable changes to this project will be documented in this file.
 
-The format is based on Keep a Changelog, and this project adheres to Semantic Versioning.
+The format is based on Keep a Changelog, and this project adheres to Semantic
+Versioning.
 
 ## [Unreleased]
 
-[Unreleased]: https://github.com/lsrcz/grisette-synth-lib/tree/main
+[unreleased]: https://github.com/lsrcz/grisette-synth-lib/tree/main
--- flake.nix
+++ flake.nix
@@ -5,75 +5,45 @@
   inputs.grisette-nix-build-env.url = "github:lsrcz/grisette-nix-build-env/main";
   inputs.grisette.url = "github:lsrcz/grisette/main";
 
-  outputs = { self, nixpkgs, flake-utils, grisette-nix-build-env, grisette }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        envLib = grisette-nix-build-env.lib.${system};
-
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            envLib.overlays.z3
-            envLib.overlays.hlintSrc
-            envLib.overlays.cvc5
-          ];
-        };
-
-        patchedHPkgs = { ghcVersion }: (envLib.patchedHaskellPackages {
-          inherit pkgs;
-          ghcVersion = ghcVersion;
-        }).extend (hfinal: hprev: {
-          grisette = pkgs.haskell.lib.dontCheck
-            grisette.packages.${system}.grisette.${ghcVersion};
-        });
-
-        hPkgs = { ghcVersion, ci }:
-          (patchedHPkgs { ghcVersion = ghcVersion; }).extend (hfinal: hprev: {
-            grisette-synth-lib = envLib.setCIOptions {
-              inherit pkgs ghcVersion hfinal ci;
-              extraTestToolDepends = [ pkgs.z3 ];
-              package = hfinal.callCabal2nix "grisette-synth-lib" ./. { };
-              mixDirs = [
-                ""
-                "spec/spec-tmp"
-              ];
-            };
-          });
-
-        devShellWithVersion = { ghcVersion, isDevelopmentEnvironment ? false }:
-          envLib.devShell {
-            inherit pkgs isDevelopmentEnvironment;
-            haskellPackages = patchedHPkgs { inherit ghcVersion; };
-            extraBuildInputs = [
-              pkgs.cairo
-              pkgs.expat
-              pkgs.pkg-config
-              pkgs.xorg.libXdmcp
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      grisette-nix-build-env,
+      grisette,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      grisette-nix-build-env.lib.${system}.output {
+        inherit nixpkgs system;
+        srcRoot = ./.;
+        extraHaskellPackages = pkgs: ghcVersion: hfinal: helpers: setCIOptions: {
+          grisette = pkgs.haskell.lib.dontCheck grisette.packages.${system}.grisette.${ghcVersion};
+          grisette-synth-lib = setCIOptions {
+            extraTestToolDepends = [ pkgs.z3 ];
+            package = hfinal.callCabal2nix "grisette-synth-lib" ./. { };
+            mixDirs = [
+              ""
+              "spec/spec-tmp"
             ];
           };
-
-        plainOutputs = envLib.defaultOutputs {
-          inherit pkgs devShellWithVersion;
-          haskellPackagesWithCiFlags = hPkgs;
-          packageName = "grisette-synth-lib";
         };
-
-        grisette-synth-lib =
-          (hPkgs { ghcVersion = "9101"; ci = false; }).grisette-synth-lib;
-      in
-      pkgs.lib.recursiveUpdate plainOutputs
-        {
-          formatter = pkgs.nixpkgs-fmt;
-
-          devShells = {
-            default = devShellWithVersion {
-              ghcVersion = "9101";
+        devShellExtraBuildInputs = pkgs: _: [
+          pkgs.cairo
+          pkgs.expat
+          pkgs.pkg-config
+          pkgs.xorg.libXdmcp
+        ];
+        pname = "grisette-synth-lib";
+        extraOutputs = pkgs: haskellPackages: devShellWithVersion: {
+          devShells.default = devShellWithVersion {
+            ghcVersion = "9101";
+            config = {
               isDevelopmentEnvironment = true;
             };
           };
-
-          packages.grisette-synth-lib = grisette-synth-lib;
-          packages.default = grisette-synth-lib;
-        });
+        };
+      }
+    );
 }
-
