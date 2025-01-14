bda360cef291eeea4049ca7789b3901179435135
Sirui Lu <siruilu@cs.washington.edu>
2025-01-14 10:19:54 -0800

    :sparkles: Add embed for Union

--- src/Grisette/Lib/Synth/Combinator/Embed.hs
+++ src/Grisette/Lib/Synth/Combinator/Embed.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Grisette.Lib.Synth.Combinator.Embed ((:<:) (..)) where
+
+import Grisette (Mergeable, Union, mrgReturn, pattern Single)
 
 class sub :<: sup where
   inj :: sub -> sup
@@ -11,3 +14,9 @@ class sub :<: sup where
 instance {-# OVERLAPPING #-} a :<: a where
   inj = id
   prj = Just
+
+instance (a :<: b, Mergeable b) => (a :<: Union b) where
+  inj = mrgReturn . inj
+  prj r = case r of
+    Single a -> prj a
+    _ -> Nothing
